60183f55537681dc09212db00f516b55c0751155
Gemma <johndoe@example.com>
2024-03-23 00:31:05 +0100

    borrar producto funcionando

--- src/components/CestaCompra.js
+++ src/components/CestaCompra.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import './CestaCompra.css'
 
-function CestaCompra ({ carrito }) {
-  const [total, setTotal] = useState(0)
-  const isMobile = window.innerWidth <= 600
-  
+function CestaCompra({ carrito, eliminarProducto }) {
+  const [total, setTotal] = useState(0);
+
+  // comprobando si se trata de una vista mobile
+  const isMobile = window.innerWidth <= 600;
 
   // calcular el precio total limitando decimales
   useEffect(() => {
     const precioTotal = carrito.reduce(
       (acum, producto) => acum + parseFloat(producto.precio),
       0
-    )
-    setTotal(parseFloat(precioTotal.toFixed(2)))
-  }, [carrito])
+    );
+    setTotal(parseFloat(precioTotal.toFixed(2)));
+  }, [carrito]);
 
+  // renderizar el componente
   return (
     <div className='container-cesta'>
       <table className='cesta-compra'>
@@ -24,6 +26,7 @@ function CestaCompra ({ carrito }) {
           </tr>
         </thead>
         <tbody className='cesta-body'>
+          {/* renderizar cada fila de producto (excluyendo vista mobile) */}
           {!isMobile &&
             carrito.map((producto, index) => (
               <tr key={index}>
@@ -36,10 +39,17 @@ function CestaCompra ({ carrito }) {
                   {producto.nombre}
                 </td>
                 <td className='precio-producto'>
+                  <button
+                    className='btn-borrar-producto'
+                    onClick={() => eliminarProducto(producto)}
+                  >
+                    <ion-icon name='trash-outline'></ion-icon>
+                  </button>
                   {parseFloat(producto.precio).toLocaleString('es-ES')} €
                 </td>
               </tr>
             ))}
+          {/* renderizar la fila con el total  */}
           <tr className='fila-total'>
             <td colSpan='2'>
               <span className='texto-total'>TOTAL</span>
@@ -48,18 +58,20 @@ function CestaCompra ({ carrito }) {
                   ({carrito.length} productos)
                 </span>
               )}
+              {/* mostrar el texto en singular si solo hay un producto */}
               {carrito.length === 1 && (
                 <span className='texto-total-productos'>
                   ({carrito.length} producto)
                 </span>
               )}
             </td>
+            {/* mostrar el precio total */}
             <td className='precio-total'>{total.toLocaleString('es-ES')} €</td>
           </tr>
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default CestaCompra
+export default CestaCompra;
--- src/components/ListaProductos.js
+++ src/components/ListaProductos.js
@@ -1,34 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import './ListaProductos.css'
+import React, { useState, useEffect } from 'react';
+import './ListaProductos.css';
 
-function ListaProductos ({ productos, carrito, agregarAlCarrito }) {
+function ListaProductos({
+  productos,
+  carrito,
+  agregarAlCarrito,
+  eliminarProducto
+}) {
   const [productosState, setProductosState] = useState(
     productos.map(producto => ({
       ...producto,
       agregado: false // inicialmente no se añade ningun producto al carro
     }))
-  )
+  );
+
+  // comprobando si se trata de una vista mobile
+  const isMobile = window.innerWidth <= 600;
 
   // la funcion comprobará si el producto está en el carrito
   const estaEnCarrito = producto => {
-    return carrito.some(item => item.nombre === producto.nombre)
-  }
+    return carrito.some(item => item.nombre === producto.nombre);
+  };
+
+  const handleEliminarProducto = producto => {
+    eliminarProducto(producto); 
+  };
 
   // función para añadir un producto al carrito
   const handleAgregarAlCarrito = producto => {
-    agregarAlCarrito(producto) // llamando la función padre
-  }
+    agregarAlCarrito(producto); // llamando la función padre
+  };
 
   useEffect(() => {
     // actualizando el estado para definir los productos agregados
-
     setProductosState(prevState =>
       prevState.map(item => ({
         ...item,
         agregado: estaEnCarrito(item)
       }))
-    )
-  }, [carrito]) //usando el hook de efecto
+    );
+  }, [carrito]); //usando el hook de efecto
 
   return (
     <div className='container-lista'>
@@ -43,10 +54,21 @@ function ListaProductos ({ productos, carrito, agregarAlCarrito }) {
                   : 'listaProductos__precio '
               }
             >
-              {parseFloat(producto.precio).toLocaleString('es-ES', {
-                minimumFractionDigits: 2
-              })}{' '}
-              €
+              <div>
+                {/* formatear el precio para dos decimales */}
+                {producto.agregado && isMobile && (
+                  <button
+                    className='btn-borrar-producto'
+                    onClick={() => handleEliminarProducto(producto)}
+                  >
+                    <ion-icon name='trash-outline'></ion-icon>
+                  </button>
+                )}
+                {parseFloat(producto.precio).toLocaleString('es-ES', {
+                  minimumFractionDigits: 2
+                })}{' '}
+                €
+              </div>
             </span>
             <span className='listaProductos__icono'>
               {!producto.agregado && (
@@ -61,6 +83,7 @@ function ListaProductos ({ productos, carrito, agregarAlCarrito }) {
                   />
                 </button>
               )}
+              {/* renderizar el botón desactivado si el producto ya se ha añadido */}
               {producto.agregado && (
                 <button disabled='true' className={'agregado-button'}>
                   <img
@@ -75,7 +98,7 @@ function ListaProductos ({ productos, carrito, agregarAlCarrito }) {
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default ListaProductos
+export default ListaProductos;
